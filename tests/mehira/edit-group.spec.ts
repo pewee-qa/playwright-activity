6e2a2e5a505fbd66fca7fd20c72ce510f9fc074b
Pewee Ventanilla <carmellajoy.ventanilla22@gmail.com>
2023-01-06 13:52:06 +0800

    test:  automation script on edit group

---
+++ tests/mehira/edit-group.spec.ts
@@ -0,0 +1,66 @@
+import { test, expect } from "@playwright/test";
+import * as data from "./source/data.json";
+import Chance from "chance";
+const chance = new Chance();
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(data.baseUrl);
+  await page
+    .locator('[data-testid="switch-form-link"]', { hasText: "Sign up" })
+    .click();
+});
+
+test("User creates a group", async ({ page }) => {
+  //  Given customer signs in to his account
+  const fname = chance.first();
+  const lname = chance.last();
+  const email = chance.email();
+
+  await page.locator('[id="first-name"]').fill(fname);
+  await page.locator('[id="last-name"]').fill(lname);
+  await page.locator('[id="emailAddress"]').fill(email);
+  await page.getByRole("button", { name: "Sign Up" }).click();
+
+  const msg = data.OTPSent;
+  await expect(page.locator(".chakra-alert__desc")).toContainText(msg);
+  const OTP = data.OTPCode;
+  const workspaceName = data.workspace;
+
+  await page.locator("#emailOtp-0").fill(OTP);
+  await page.locator("#emailOtp-1").fill(OTP);
+  await page.locator("#emailOtp-2").fill(OTP);
+  await page.locator("#emailOtp-3").fill(OTP);
+  await page.locator("#emailOtp-4").fill(OTP);
+  await page.locator("#emailOtp-5").fill(OTP);
+  await page.getByRole("button", { name: "Continue" }).click();
+  await page.locator('[name="name"]').fill(workspaceName);
+  await page.getByRole("button", { name: "Continue" }).click();
+
+  //When user wants to edit a group
+  await page.goto(data.baseUrl + "groups");
+  await page.locator("button", { hasText: "Create Group" }).click();
+
+  const groupName = chance.name();
+  const description = data.description;
+
+  await page.locator('[data-testid="input-name"]').type(groupName);
+  await page.locator('[name="description"]').type(description);
+
+  await page.locator("button", { hasText: "Submit" }).click();
+
+  const promptMessage = data.promptMessage;
+  await expect(page.locator(".chakra-alert__desc")).toContainText(
+    promptMessage
+  );
+
+  //Then user is redirected to the Group details modal
+  await page
+    .locator(':nth-match([data-testid="table-menu-button"], 1)')
+    .click();
+  await page
+    .locator('[data-testid="table-menu-view"]', { hasText: "View" })
+    .click();
+  await page.locator("button", { hasText: "Edit" }).click();
+
+  await page.getByRole("dialog", { name: "Group details" }).isVisible;
+});
